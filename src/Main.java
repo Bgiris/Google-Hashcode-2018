b50d07463b76a74dd0fcf6b191e226b25a0bd75d
Berkay Giriş <berkaygiris@gmail.com>
2018-03-01 19:44:38 +0300

    Read Write

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,22 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 
 public class Main {
 	
-	public static void main(String[] args) throws IOException {
-		
+	private static void FileWrite(String filename) throws FileNotFoundException, UnsupportedEncodingException {
+		PrintWriter writer = new PrintWriter("./output/"+filename+".out", "UTF-8");
+		writer.println("The first line");
+		writer.println("The second line");
+		writer.close();
+	}
+	
+	private static void FileRead(String filename) throws IOException{
 		String everything;
-		BufferedReader br = new BufferedReader(new FileReader("./input/small.in"));
+		BufferedReader br = new BufferedReader(new FileReader("./input/"+filename+".in"));
 		try {
 		    StringBuilder sb = new StringBuilder();
 		    String line = br.readLine();
@@ -28,6 +37,17 @@ public class Main {
 			System.out.println("hehe");
 			System.out.println(data[0]);
 		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		
+		// INPUT FILE NAME
+		BufferedReader brin = new BufferedReader(new InputStreamReader(System.in));
+		String filename = brin.readLine();
+		
+		// READ
+		FileRead(filename);
+		FileWrite(filename);
 		
 	}
 
