8e9e5de5ed936b50fd1d177a427b0ba48f5f3116
birkandenizer <denizerb@itu.edu.tr>
2018-03-01 21:26:59 +0300

    Fixed Ride style

--- src/Ride.java
+++ src/Ride.java
@@ -1,5 +1,5 @@
 
-public class Rides {
+public class Ride {
 	
 	public Position startPos;
 	public Position finishPos;
@@ -8,7 +8,7 @@ public class Rides {
 	public Integer distance;
 	public boolean isEmpty;
 	
-	public Rides(Position _startPos, Position _finishPos, Integer _startTime, Integer _finishTime) {
+	public Ride(Position _startPos, Position _finishPos, Integer _startTime, Integer _finishTime) {
 		this.startPos = _startPos;
 		this.finishPos = _finishPos;
 		this.startTime = _startTime;
