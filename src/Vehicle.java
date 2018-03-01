35e79d8f0aedc84fdf284e468f638f50fb3999c9
Berkay Giriş <berkaygiris@gmail.com>
2018-03-01 21:28:51 +0300

    vehicle constructor

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -3,10 +3,11 @@ import java.util.ArrayList;
 public class Vehicle {
 	public Position currentPos;
 	public boolean isEmpty;
-	public ArrayList<String> rideList = new ArrayList<String>();
+	public ArrayList<String> rideList;
 	
-	public Vehicle(Position current) {
-		this.currentPos = current;
+	public Vehicle() {
+		this.rideList = new ArrayList<String>();
+		this.currentPos = new Position(0,0);
 		this.isEmpty = true;
 	}
 }
