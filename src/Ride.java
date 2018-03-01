5b3ada4a29939edd1755bee34b0edecf4336fcff
birkandenizer <denizerb@itu.edu.tr>
2018-03-01 21:41:19 +0300

    Added id attribute and toString method to Ride class

--- src/Ride.java
+++ src/Ride.java
@@ -1,6 +1,7 @@
 
 public class Ride {
 	
+	public Integer id;
 	public Position startPos;
 	public Position finishPos;
 	public Integer startTime;
@@ -8,12 +9,23 @@ public class Ride {
 	public Integer distance;
 	public boolean isEmpty;
 	
-	public Ride(Position _startPos, Position _finishPos, Integer _startTime, Integer _finishTime) {
+	public Ride(Integer _id, Position _startPos, Position _finishPos, Integer _startTime, Integer _finishTime) {
+		this.id = _id;
 		this.startPos = _startPos;
 		this.finishPos = _finishPos;
 		this.startTime = _startTime;
 		this.finishTime = _finishTime;
 		this.distance = Math.abs(_finishPos.x - _startPos.x) + Math.abs(_finishPos.y - _startPos.y);
 		this.isEmpty = false;
+	}
+	
+	public String toString() {
+		return "Id:" + this.id + "   "
+				+ "Start position:" + this.startPos.x + ", " + this.startPos.y + "   "
+                + "finish position:" + this.finishPos.x + ", " + this.finishPos.y + "   "
+                + "start time:" + this.startTime + "  " 
+                + "finish time:" + this.finishTime + "   "
+                + "distance:" + this.distance + "  " 
+                + "isEmpty:" + this.isEmpty;
 	}
 }
