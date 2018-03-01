c0bdd46a93131b7dda2486e98c2ce633c77baccc
Berkay Giriş <berkaygiris@gmail.com>
2018-03-01 21:36:34 +0300

    Vehicle id

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -4,8 +4,9 @@ public class Vehicle {
 	public Position currentPos;
 	public boolean isEmpty;
 	public ArrayList<String> rideList;
+	public Integer id;
 	
-	public Vehicle() {
+	public Vehicle(Integer id) {
 		this.rideList = new ArrayList<String>();
 		this.currentPos = new Position(0,0);
 		this.isEmpty = true;
