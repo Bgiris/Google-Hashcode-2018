3e0518aead4c0c100fd726c2646673ec1ea7921a
birkandenizer <denizerb@itu.edu.tr>
2018-03-01 21:22:27 +0300

    Implemented Rides class

---
+++ src/Rides.java
@@ -0,0 +1,19 @@
+
+public class Rides {
+	
+	public Position startPos;
+	public Position finishPos;
+	public Integer startTime;
+	public Integer finishTime;
+	public Integer distance;
+	public boolean isEmpty;
+	
+	public Rides(Position _startPos, Position _finishPos, Integer _startTime, Integer _finishTime) {
+		this.startPos = _startPos;
+		this.finishPos = _finishPos;
+		this.startTime = _startTime;
+		this.finishTime = _finishTime;
+		this.distance = Math.abs(_finishPos.x - _startPos.x) + Math.abs(_finishPos.y - _startPos.y);
+		this.isEmpty = false;
+	}
+}
